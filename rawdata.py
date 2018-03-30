bf44697771c06b0921806c8bd5db899208001a72
Roushd Ahmed <roushd_ahmed@hotmail.com>
2018-03-30 13:26:04 -0400

    Tweet streaming with sentiment analysis

--- rawdata.py
+++ rawdata.py
@@ -14,6 +14,15 @@ ACCESS_SECRET_TOKEN = ''
 
 STOCK_NAME = 'TSLA'
 
+analyzer = vaderSentiment.SentimentIntensityAnalyzer()
+
+def generate_stocks_csv(ts):
+    stock_data, meta_data = ts.get_daily(STOCK_NAME, outputsize='full')
+    #stock_data.drop('2. high', 1)
+    del stock_data['2. high']
+    del stock_data['1. open']
+    del stock_data['3. low']
+    stock_data.to_csv(STOCK_NAME + '.csv', sep=',')
 
 def cleanse_tweet(tweet):
     # process the tweets
@@ -23,7 +32,7 @@ def cleanse_tweet(tweet):
     #Convert www.* or https?://* to URL
     tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
     #Convert @username to AT_USER
-    tweet = re.sub('@[^\s]+','AT_USER',tweet)
+    tweet = re.sub('@[^\s]+','username',tweet)
     #Remove additional white spaces
     tweet = re.sub('[\s]+', ' ', tweet)
     #Replace #word with word
@@ -32,12 +41,21 @@ def cleanse_tweet(tweet):
     tweet = tweet.strip('\'"')
     return tweet
 
-def vader_sentiment(tweet):
-    return tweet
+class TwitterStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        clean_status = cleanse_tweet(status.text)
+        vs = analyzer.polarity_scores(clean_status)
+        print(clean_status)
+        print(vs)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_error disconnects the stream
+            return False
 
 if __name__ == '__main__':
     data = json.loads(open('API_KEYS.json', 'r').read())
-    analyzer = vaderSentiment.SentimentIntensityAnalyzer()
 
     ALPHA_API_KEY = data['ALPHA_API_KEY']
     CONSUMER_KEY = data['CONSUMER_KEY']
@@ -46,17 +64,15 @@ if __name__ == '__main__':
     ACCESS_SECRET_TOKEN = data['ACCESS_SECRET_TOKEN']
 
     ts = TimeSeries(key=ALPHA_API_KEY, output_format='pandas')
-    stock_data, meta_data = ts.get_daily(STOCK_NAME, outputsize='full')
-    #stock_data.drop('2. high', 1)
-    del stock_data['2. high']
-    del stock_data['1. open']
-    del stock_data['3. low']
-    stock_data.to_csv(STOCK_NAME + '.csv', sep=',')
-
+    generate_stocks_csv(ts)
 
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY);
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET_TOKEN);
     api = tweepy.API(auth);
+
+    stream_listener = TwitterStreamListener()
+    stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+    stream.filter(track=['Tesla', 'tesla', 'TSLA'])
 
 
     public_tweets = api.search('Tesla');
@@ -64,5 +80,3 @@ if __name__ == '__main__':
     for tweet in public_tweets:
         clean_tweet = cleanse_tweet(tweet.text)
         print(clean_tweet)
-        vs = analyzer.polarity_scores(clean_tweet)
-        print(vs)
